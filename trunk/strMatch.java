a60e62109828fd0c2ee61efd4b258cd9c387f8a5
davidkraftishihama <davidkraftishihama@c632e10b-3c5b-4084-b371-9936f49cfd65>
2010-04-30 18:34:20 +0000

    stuff

--- trunk/strMatch.java
+++ trunk/strMatch.java
@@ -1,7 +1,45 @@
 import java.util.*;
 import java.io.*;
+import java.util.regex.*;
 
 public class strMatch  {
+        //Copied from our FSM project
+        public static String read(String inFile) throws Exception {
+
+	FileInputStream in = new FileInputStream(inFile);
+	StringBuffer input = new StringBuffer();
+	//DataInputStream dataIn = new DataInputStream(in);
+        
+	while(in.available() > 0){
+	 input.append( (char)in.read());
+	 }
+   	 in.close();
+   	 return input.toString();
+	}//end read
+	
+	//David is driving
+	public static ArrayList<String> parsePattern(String patterns) {
+		Pattern pat = Pattern.compile("&([^&])*&");
+		Matcher mat = pat.matcher(patterns);
+		ArrayList<String> patternList = new ArrayList<String>();
+		while (mat.find() ) {
+			String pattern = mat.group();
+			pattern = pattern.substring(1, pattern.length() - 1);
+			patternList.add(pattern);
+		}
+		return patternList;
+	}
+
 	public static void main(String[] args) {
+		
+		
+		
 	} //end main
+	
+	//Marcell driving
+	public static boolean RK(String pattern, String text) {
+		int hashValue = hash(pattern);
+		
+		return false;
+	}
 } //end strMatch
--- trunk/strMatchTest.java
+++ trunk/strMatchTest.java
@@ -1,10 +1,16 @@
+import java.util.*;
 import org.junit.* ;
 import static org.junit.Assert.*;
 
 public class strMatchTest {
+        //David is driving
 	@Test
 	public void test1() {
-        assertEquals(1, 1); 
+	ArrayList<String> test = strMatch.parsePattern(new String("&abcde&   &waqa&   &afweaaq\nwera\nqera&zelda&poop&"));
+	assertTrue((test.get(0)).equals(new String("abcde"))); 
+        assertTrue((test.get(1)).equals(new String("waqa")));
+        assertTrue((test.get(2)).equals(new String("afweaaq\nwera\nqera"))); 
+        assertTrue((test.get(3)).equals(new String("poop"))); 
     	} //end test1
     	
 } //end strMatchTest
