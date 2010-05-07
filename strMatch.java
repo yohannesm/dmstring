4159ada614ca72fd8e9084b3063e0efb27a80353
davidkraftishihama <davidkraftishihama@c632e10b-3c5b-4084-b371-9936f49cfd65>
2010-05-07 20:13:50 +0000

    fwaekpofkawopekfopawkefopawkefopkaw

--- strMatch.java
+++ strMatch.java
@@ -6,6 +6,7 @@ import java.util.regex.*;
 public class strMatch  {
         
   final static int BUF_SIZE = 2000;
+  final static int P = 1117;
 	static int hashValue = 0;
         //Copied from our FSM project
         public static String readPattern(String inFile) throws Exception {
@@ -95,14 +96,18 @@ String.valueOf(end - begin));
 	     if(q.size() == pLen){
 	       Character c1 = q.remove();
 	       int temp1 = c1.charValue();
-	       hashValue -= (temp1 *pLen * 256); 
+	       temp1 = (moduArith(256, pLen - 1 , P) * temp1) % P;
+	       hashValue = (hashValue - temp1) % P;
+	       while (hashValue < 0) hashValue += P;
+	    
 	     }
+	     hashValue = (hashValue * 256) % P;
+	 
 	     int cInt =  c;
-	     hashValue += (cInt * ind * 256);
+	     hashValue = (hashValue + cInt) % P;
 	     q.offer( new Character(c) );
-	     if(q.size() == pLen) hashValue *= 256;
 	     assert hashValue>=0;
-	   return hashValue % 1117;
+	   return hashValue;
 	}
 
 	public static int hashPattern(String str){
@@ -110,7 +115,7 @@ String.valueOf(end - begin));
 	    Queue<Character> q = new LinkedList<Character>();
 	    int result = 0;
             for(int i=0; i<str.length(); i++){
-	       result = hash(str.length(), q, str.charAt(i) ); 
+	       result = hash1(str.length(), q, str.charAt(i) ); 
 	    }
 	    hashValue = 0;
 	    return result;
@@ -120,6 +125,7 @@ String.valueOf(end - begin));
 	public static boolean RK(String pattern, String inFile) throws Exception{
 	if (pattern.length() == 0) return true;
         int hashPat = hashPattern(pattern);
+        
 	FileInputStream in = new FileInputStream(inFile);
 	StringBuffer input = new StringBuffer();
 
@@ -140,7 +146,7 @@ String.valueOf(end - begin));
 		
   	int m = pattern.length();
         Queue<Character> q1 = new LinkedList<Character>();
-  	for(int i =0; i< text.length(); i++){
+  	for(int i =0; i< text.length(); i++){ 
 	if(in.available() >0 && (i==text.length()-1 )){
 	     input.delete(0, BUF_SIZE/2 );
 	while(in.available() > 0 && ( input.length() < BUF_SIZE) ){
@@ -159,7 +165,8 @@ String.valueOf(end - begin));
 	text = input.toString();
 	}
   	int j = 0;
-	hash(m, q1, text.charAt(i));
+	hash1(m, q1, text.charAt(i));
+
 	       if(hashValue == hashPat && (i-m+1>= 0)){
      		for(j=0; j<m && pattern.charAt(j)==text.charAt(i-m+1+j); j++){
      		}
@@ -251,4 +258,30 @@ String.valueOf(end - begin));
 	  return result;
 	}
 
+        // This function was copied from project 2 with minor modifications
+        public static int moduArith(int M, int e, int n){
+                if ( e == 0 ) return 1;                
+		assert e > 0;
+		assert n > 0;
+		assert M >= 0;
+		int c = 1, h = 0;
+		//bitTest is a bitmask for bi
+		int bitTest = 1 << 30;
+		//shift all the way for the most significant bit of e
+		while( (bitTest & e) == 0){
+		  bitTest = bitTest >>> 1;
+		}
+
+		for(; bitTest!=0; bitTest = bitTest >>> 1){
+   		if( (bitTest & e) ==0){
+		      c = (c*c) % n;
+		   }
+		   else{
+		      c = (((c*c) % n) * M ) % n;
+		   }
+}
+
+return c;
+
+}// end moduArith
 } //end strMatch
